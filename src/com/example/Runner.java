6c3c8a003392574e5e9201dcd49bfe3e875e95ef
Mina <carmina.opada@orangeandbronze.com>
2014-05-29 16:02:53 +0800

    third sysout lyrics

--- src/com/example/Runner.java
+++ src/com/example/Runner.java
@@ -5,6 +5,8 @@ public class Runner {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("second hello");
+		
+		System.out.println("Can't stop, won't stop; I must be dreaming");
 	}
 
 }
